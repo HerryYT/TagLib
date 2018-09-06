8704cc1b4498b63df9f2a64cfd5a21b6dad44c21
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2018-09-06 20:08:54 +0200

    Move stuff over to a more direct method (byte to primitive)

--- src/main/java/io/gomint/taglib/NBTStreamReaderNoBuffer.java
+++ src/main/java/io/gomint/taglib/NBTStreamReaderNoBuffer.java
@@ -42,8 +42,15 @@ public class NBTStreamReaderNoBuffer {
 
     protected short readShortValue() throws IOException {
         this.expectInput( 2, "Invalid NBT Data: Expected short" );
-        byte[] data = new byte[]{this.readByteValue(), this.readByteValue()};
-        return ByteBuffer.wrap( data ).order( this.byteOrder ).getShort();
+
+        byte[] data = new byte[2];
+        this.in.read( data );
+
+        if ( this.byteOrder == ByteOrder.BIG_ENDIAN ) {
+            return (short) ( ( data[0] << 8 ) | ( data[1] & 0xff ) );
+        }
+
+        return (short) ( ( data[1] << 8 ) | ( data[0] & 0xff ) );
     }
 
     protected int readIntValue() throws IOException {
@@ -54,7 +61,18 @@ public class NBTStreamReaderNoBuffer {
         this.expectInput( 4, "Invalid NBT Data: Expected int" );
         byte[] data = new byte[4];
         this.in.read( data );
-        return ByteBuffer.wrap( data ).order( this.byteOrder ).getInt();
+
+        if ( this.byteOrder == ByteOrder.BIG_ENDIAN ) {
+            return ( ( ( data[0] ) << 24 ) |
+                    ( ( data[1] & 0xff ) << 16 ) |
+                    ( ( data[2] & 0xff ) << 8 ) |
+                    ( data[3] & 0xff ) );
+        }
+
+        return ( ( ( data[3] ) << 24 ) |
+                ( ( data[2] & 0xff ) << 16 ) |
+                ( ( data[1] & 0xff ) << 8 ) |
+                ( data[0] & 0xff ) );
     }
 
     protected long readLongValue() throws IOException {
@@ -64,30 +82,44 @@ public class NBTStreamReaderNoBuffer {
             this.expectInput( 8, "Invalid NBT Data: Expected long" );
             byte[] data = new byte[8];
             this.in.read( data );
-            return ByteBuffer.wrap( data ).order( this.byteOrder ).getLong();
+
+            if ( this.byteOrder == ByteOrder.BIG_ENDIAN ) {
+                return ( ( ( (long) data[0] ) << 56 ) |
+                        ( ( (long) data[1] & 0xff ) << 48 ) |
+                        ( ( (long) data[2] & 0xff ) << 40 ) |
+                        ( ( (long) data[3] & 0xff ) << 32 ) |
+                        ( ( (long) data[4] & 0xff ) << 24 ) |
+                        ( ( (long) data[5] & 0xff ) << 16 ) |
+                        ( ( (long) data[6] & 0xff ) << 8 ) |
+                        ( (long) data[7] & 0xff ) );
+            }
+
+            return ( ( ( (long) data[7] ) << 56 ) |
+                    ( ( (long) data[6] & 0xff ) << 48 ) |
+                    ( ( (long) data[5] & 0xff ) << 40 ) |
+                    ( ( (long) data[4] & 0xff ) << 32 ) |
+                    ( ( (long) data[3] & 0xff ) << 24 ) |
+                    ( ( (long) data[2] & 0xff ) << 16 ) |
+                    ( ( (long) data[1] & 0xff ) << 8 ) |
+                    ( (long) data[0] & 0xff ) );
         }
     }
 
     protected float readFloatValue() throws IOException {
         this.expectInput( 4, "Invalid NBT Data: Expected float" );
-        byte[] data = new byte[]{this.readByteValue(), this.readByteValue(), this.readByteValue(), this.readByteValue()};
-        return ByteBuffer.wrap( data ).order( this.byteOrder ).getFloat();
+        return Float.intBitsToFloat( this.readIntValue() );
     }
 
     protected double readDoubleValue() throws IOException {
         this.expectInput( 8, "Invalid NBT Data: Expected double" );
-        byte[] data = new byte[]{this.readByteValue(), this.readByteValue(), this.readByteValue(), this.readByteValue(),
-                this.readByteValue(), this.readByteValue(), this.readByteValue(), this.readByteValue()};
-        return ByteBuffer.wrap( data ).order( this.byteOrder ).getDouble();
+        return Double.longBitsToDouble( this.readLongValue() );
     }
 
     protected byte[] readByteArrayValue() throws IOException {
         int size = this.readIntValue();
         this.expectInput( size, "Invalid NBT Data: Expected byte array data" );
         byte[] data = new byte[size];
-        for ( int i = 0; i < size; i++ ) {
-            data[i] = this.readByteValue();
-        }
+        this.in.read( data );
         return data;
     }
 
